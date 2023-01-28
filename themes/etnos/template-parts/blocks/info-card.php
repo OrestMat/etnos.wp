4aa32d0ce321d45a9a88788f1e099e857509717b
vrsavka <vasyl.s.upqode@gmail.com>
2023-01-28 20:25:01 +0200

    add to#info-card

--- themes/etnos/template-parts/blocks/info-card.php
+++ themes/etnos/template-parts/blocks/info-card.php
@@ -4,5 +4,17 @@ global $page_block;
 
 ?>
 
-
-<h1>adsads</h1>
+<section class="info-card">
+	<div class="container">
+		<div class="info-card__row">
+			<div class="info-card__content">
+				<h2><?php echo $page_block['title']?></h2>
+				<div class="info-card__subtitle"><?php echo $page_block['subtitle']?></div>
+				<div><?php echo $page_block['text']?></div>
+			</div>
+			<div class="info-card__img">
+				<?php echo wp_get_attachment_image( $page_block['image']['id'], 'full' ); ?>
+			</div>
+		</div>
+	</div>
+</section>
