45fbccf5bde77c798a4533bcab5306e00d0418e2
vrsavka <vasyl.s.upqode@gmail.com>
2023-01-28 20:29:21 +0200

    add to #info-card

--- themes/etnos/template-parts/blocks/info-card.php
+++ themes/etnos/template-parts/blocks/info-card.php
@@ -9,8 +9,8 @@ global $page_block;
 		<div class="info-card__row">
 			<div class="info-card__content">
 				<h2><?php echo $page_block['title']?></h2>
-				<div class="info-card__subtitle"><?php echo $page_block['subtitle']?></div>
-				<div><?php echo $page_block['text']?></div>
+				<p class="info-card__subtitle"><?php echo $page_block['subtitle']?></p>
+				<p><?php echo $page_block['text']?></p>
 			</div>
 			<div class="info-card__img">
 				<?php echo wp_get_attachment_image( $page_block['image']['id'], 'full' ); ?>
