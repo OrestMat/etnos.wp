1977145e8b9f35051b21a1f4c0f7378f1045aa7f
makes0menoize <bykovskykh.o@gmail.com>
2023-04-02 17:36:45 +0300

    made foreach

--- themes/etnos/page-layouts/advantage/advantage.php
+++ themes/etnos/page-layouts/advantage/advantage.php
@@ -1,7 +1,38 @@
+<?php
+global $page_block;
+var_dump($page_block);
+
+
+?>
+
+
 <section class="etnos-advantage">
     <div class="container">
         <div class="etnos-advantage__wrapper">
-
+            <?php foreach ($page_block['item'] as $item) { ?>
+                <div class="etnos-advantage__item">
+                    <div class="etnos-advantage__image <?php echo ($item['icon']) ?>"></div>
+                    <div class="etnos-advantage__content">
+                        <h3><?php echo $item['title'] ?></h3>
+                        <p><?php echo $item['description'] ?></p>
+                    </div>
+                </div>
+                <div class="etnos-advantage__decor"></div>
+                <div class="etnos-advantage__item">
+                    <div class="etnos-advantage__image <?php echo ($item['icon_2']) ?>"></div>
+                    <div class="etnos-advantage__content">
+                        <h3><?php echo $item['title_2'] ?></h3>
+                        <p><?php echo $item['description_2'] ?></p>
+                    </div>
+                </div>
+                <div class="etnos-advantage__decor"></div>
+                <div class="etnos-advantage__item">
+                    <div class="etnos-advantage__image <?php echo ($item['icon_3']) ?>"></div>
+                    <div class="etnos-advantage__content">
+                        <h3><?php echo $item['title_3'] ?></h3>
+                        <p><?php echo $item['description_3'] ?></p>
+                    </div>
+                </div>
+            <?php } ?>
         </div>
-    </div>
 </section>
