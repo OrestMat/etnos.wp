7baac5daad7f8a80c437323cc0922ec2b18461d0
Orest Matsukh <orest.sd@gmail.com>
2023-07-15 02:05:58 +0300

    add

--- themes/etnos/functions.php
+++ themes/etnos/functions.php
@@ -265,3 +265,25 @@ function get_cart_count()
 
 
 add_filter('woocommerce_cart_needs_shipping_address', '__return_false');
+
+
+add_filter('woocommerce_currency_symbol', 'add_my_currency_symbol', 10, 2);
+
+add_filter('woocommerce_currencies', 'add_my_currency');
+function add_my_currency($currencies)
+{
+  $currencies['UAH'] = __('Українська гривня', 'woocommerce');
+  return $currencies;
+}
+
+
+add_filter('woocommerce_currency_symbol', 'add_my_currency_symbol', 10, 2);
+function add_my_currency_symbol($currency_symbol, $currency)
+{
+  switch ($currency) {
+    case 'UAH':
+      $currency_symbol = 'грн';
+      break;
+  }
+  return $currency_symbol;
+}
